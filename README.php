42a89b15fe4d1bec9f40871371c9c22c65e9d656
omarcazares2007 <161668264+omarcazares2007@users.noreply.github.com>
2024-03-05 21:53:14 -0800

    Este es el primero avance de confirmacion

--- README.php
+++ README.php
@@ -1,6 +1,37 @@
 <php
+  {
+  $Objeto” = Array(“Computadora”); 
+ 
+“Atributo1” => “Procesador (CPU)”; 
+“Atributo2” => “Memoria RAM”;
+“Atributo3” => “Disco duro (almacenamiento)”;
+“Atributo4” => “Tarjeta gráfica (GPU)”; 
+“Atributo5”=> “Pantalla (monitor)”;
+“Atributo6”  => “Teclado”,
+“Atributo7”  => “Ratón (o touchpad)”,
+“Atributo8”  => “Puertos USB”,
+“Atributo9”  => “Conectividad de red (Ethernet, Wi-Fi, Bluetooth)”,
+“Atributo10” => “Sistema operativo”,
+“Atributo11”=> “ Tarjeta de sonido”,
+“Atributo12” => “Unidad óptica “,
+“Atributo13”=> “Batería”,
+“Atributo14”=> “Cámara web”,
+“Atributo15” => “Altavoces”,
+“Atributo16” => “Micrófono”,
+“Atributo17”=> “Sistema de enfriamiento (ventiladores, disipadores de calor)”,
+“Atributo18” => “Puerto de alimentación”,
+“Atributo19” => “Software preinstalado “,
+“Atributo20” => “Puertos de video”,
 
-$Objeto = ("computadora");
-echo  =("ventilador","
-echo = "($Objeto)",
+echo = "($Objeto)";
+  }
+}
+
+
+
+
+
+
+
+  
 
